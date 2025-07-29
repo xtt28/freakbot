8e2bf0b2502205512d08f2b65c74e8bf2388aa4a
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-07-29 06:36:15 -0400

    Make /leaderboard command create leaderboard if not existing

--- internal/commands/leaderboard.go
+++ internal/commands/leaderboard.go
@@ -1,38 +1,61 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/xtt28/freakbot/internal/manifest"
+	"gorm.io/gorm"
 )
 
 func (r *CommandRegistry) Leaderboard(s *discordgo.Session, i *discordgo.Interaction) {
 	lbID, err := r.dbConn.LeaderboardRepository().GetLeaderboardID(i.GuildID)
 	if err != nil {
-		log.Println("could not get leaderboard ID for guild " + i.GuildID, err)
-		s.InteractionRespond(i, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Couldn't find the leaderboard for this guild. This shouldn't be happening... try again or contact a bot administrator for help.",
-			},
-		})
+		log.Println("could not get leaderboard ID for guild "+i.GuildID, err)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			err := r.dbConn.LeaderboardRepository().CreateLeaderboard(i.GuildID)
+			if err != nil {
+				log.Println("could not create leaderboard for guild "+i.GuildID, err)
+				s.InteractionRespond(i, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "There's no leaderboard for this guild and we couldn't create one. This shouldn't be happening... make an issue on GitHub " + manifest.GitHubURL,
+					},
+				})
+			}
+			s.InteractionRespond(i, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "We just created the leaderboard for this guild. Please try again.",
+				},
+			})
+		} else {
+			s.InteractionRespond(i, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Couldn't find this guild's leaderboard. This shouldn't be happening... make an issue on GitHub " + manifest.GitHubURL,
+				},
+			})
+		}
+		return
 	}
 	lb, err := r.dbConn.LeaderboardRepository().GetEntries(lbID, 10, 0)
 	if err != nil {
-		log.Println("could not get leaderboard entries for guild " + i.GuildID, err)
+		log.Println("could not get leaderboard entries for guild "+i.GuildID, err)
 		s.InteractionRespond(i, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Content: "Couldn't find the leaderboard entries for this guild. This shouldn't be happening... try again or contact a bot administrator for help.",
 			},
 		})
+		return
 	}
 
 	responseBody := "Top 10 freakiest members\n\n"
 	for i, v := range lb {
-		responseBody += fmt.Sprintf("**%d.** <@%s> (%d freaky messages)\n", i + 1, v.UserID, v.FlaggedMessageCount)
+		responseBody += fmt.Sprintf("**%d.** <@%s> (%d freaky messages)\n", i+1, v.UserID, v.FlaggedMessageCount)
 	}
 
 	s.InteractionRespond(i, &discordgo.InteractionResponse{
